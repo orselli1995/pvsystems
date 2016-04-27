1cac4c4358ec34ed5bd009c85b2cca95aff29ff0
Joe Orselli <Joe Orselli>
2016-04-27 11:14:59 +0200

    Look at my first function

---
+++ orselli_function1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is an test
+
+@author: Joe
+
+input:
+x .- float value
+output:
+y .- float value
+"""
+
+def function1(x):
+    y = 2*x+10
+    return y
+    
+    
